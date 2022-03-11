9ea9d4d7ca456419ea84fde174fe0eb4f0242f98
Rory Fewell <roryfewell@hotmail.com>
2022-03-11 17:21:55 +0000

    Prelim: Base Professional theme from Luna (Blue) - builds

---
+++ themes/professional/themevars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export THEME_BASENAME="Professional"
+export THEME_DESCRIPTION="Professional visual style for XFCE."
+export THEME_MAINTAINER="Rory Fewell <roryf@oddmatics.uk>"
+export THEME_RAWNAME="professional"
+export THEME_VARIANT="Default"
+
+export THEME_DISPLAYNAME="${THEME_BASENAME}"
