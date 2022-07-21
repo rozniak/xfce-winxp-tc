31d39a934a9c96c9e6731d22ea600c5c65b010b7
Rory Fewell <roryfewell@hotmail.com>
2022-07-21 22:44:09 +0100

    Prelim: Begin native work, add colour schemes

---
+++ themes/native/themevars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export THEME_BASENAME="Windows Classic"
+export THEME_DESCRIPTION="Windows Classic visual style for XFCE."
+export THEME_MAINTAINER="Rory Fewell <roryf@oddmatics.uk>"
+export THEME_RAWNAME="native"
+export THEME_VARIANT="Default"
+
+export THEME_DISPLAYNAME="${THEME_BASENAME}"
