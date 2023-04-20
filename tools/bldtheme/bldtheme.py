2e438ab6b9f1a065c2462d05c1efa5e78ffaefa2
Rory Fewell <roryf@oddmatics.uk>
2023-04-20 22:36:57 +0100

    Prelim: bldtheme: just copy over .static.png files

--- tools/bldtheme/bldtheme.py
+++ tools/bldtheme/bldtheme.py
@@ -1,6 +1,7 @@
 import argparse
 import fnmatch
 import os
+import shutil
 import sys
 
 from PIL import Image, ImageDraw
@@ -16,7 +17,7 @@ def main():
 
     for filename in os.listdir(args.inputdir):
         if fnmatch.fnmatch(filename, "*.src.png"):
-            basename = filename[0:filename.find(".src.png")]
+            basename = get_basename(filename)
             maskname = f"{basename}.mask.png"
             pngname = filename
 
@@ -30,10 +31,17 @@ def main():
             drawing = ImageDraw.Draw(newImage)
 
             drawing.bitmap((0, 0), maskMono, "rgb(128,128,128)")
-            
+
             composed = Image.alpha_composite(newImage, pngRGB)
 
             composed.save(os.path.join(args.outputdir, f"{basename}.png"))
+        elif fnmatch.fnmatch(filename, "*.static.png"):
+            basename = get_basename(filename)
+
+            shutil.copyfile(os.path.join(args.inputdir, filename), os.path.join(args.outputdir, f"{basename}.png"))
+
+def get_basename(filename):
+    return filename[0:filename.find(".")]
 
 if __name__ == "__main__":
     main()
