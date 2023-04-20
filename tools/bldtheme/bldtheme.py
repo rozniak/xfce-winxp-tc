0183b2e6f7483df9a6e01abe246ed4e55d89379e
Rory Fewell <roryf@oddmatics.uk>
2023-04-21 00:32:52 +0100

    Prelim: Updated handling of colour scheme constants

--- tools/bldtheme/bldtheme.py
+++ tools/bldtheme/bldtheme.py
@@ -10,10 +10,13 @@ from PIL import Image, ImageDraw
 def main():
     parser = argparse.ArgumentParser(prog="Theme Part Composer Utility", description="Composes theme part graphics from source files.", epilog="See README.MD if you want to know the details.")
 
+    parser.add_argument("scheme", help="path to the scheme file")
     parser.add_argument("inputdir", help="the input directory to scan for files")
     parser.add_argument("outputdir", help="the output directory to place composed files")
 
     args = parser.parse_args()
+
+    scheme = parse_scheme(args.scheme)
 
     for filename in os.listdir(args.inputdir):
         if fnmatch.fnmatch(filename, "*.src.png"):
@@ -30,7 +33,10 @@ def main():
 
             drawing = ImageDraw.Draw(newImage)
 
-            drawing.bitmap((0, 0), maskMono, "rgb(128,128,128)")
+            if "progress" in basename:
+                drawing.bitmap((0, 0), maskMono, scheme["ACTIVE_TITLE_BAR_BG1"])
+            else:
+                drawing.bitmap((0, 0), maskMono, scheme["THREED_OBJECTS_BG"])
 
             composed = Image.alpha_composite(newImage, pngRGB)
 
@@ -42,6 +48,23 @@ def main():
 
 def get_basename(filename):
     return filename[0:filename.find(".")]
+
+def parse_scheme(path):
+    scheme = dict()
+
+    with open(path) as schemefile:
+        for line in schemefile:
+            if line == "":
+                continue
+
+            equals_pos = line.find("=")
+
+            scheme_id = line[0:equals_pos]
+            scheme_value = line[equals_pos + 1:len(line)]
+
+            scheme[scheme_id] = scheme_value
+
+    return scheme
 
 if __name__ == "__main__":
     main()
