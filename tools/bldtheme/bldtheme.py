f1639785982a1a95797e5f6e2670e59e64dc144d
John Kilpatrick <john.kilpatrick@outlook.com>
2023-04-21 20:19:58 +0100

    Cleaning: Improvements to bldtheme.py

--- tools/bldtheme/bldtheme.py
+++ tools/bldtheme/bldtheme.py
@@ -1,14 +1,24 @@
 import argparse
 import fnmatch
-import os
 import shutil
-import sys
 
+from pathlib import Path
 from PIL import Image, ImageDraw
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="Theme Part Composer Utility", description="Composes theme part graphics from source files.", epilog="See README.MD if you want to know the details.")
+    """Composes theme part graphics from source files.
+
+    Parses command line arguments for the path to the scheme file,
+    the input directory to scan for files, and the output directory to place
+    composed files. It then reads the scheme file and uses it to compose theme part
+    graphics from source files.
+    """
+    parser = argparse.ArgumentParser(
+        prog="Theme Part Composer Utility",
+        description="Composes theme part graphics from source files.",
+        epilog="See `./README.MD` for details.",
+    )
 
     parser.add_argument("scheme", help="path to the scheme file")
     parser.add_argument("inputdir", help="the input directory to scan for files")
@@ -16,17 +26,27 @@ def main():
 
     args = parser.parse_args()
 
-    scheme = parse_scheme(args.scheme)
+    scheme = parse_scheme(Path(args.scheme))
+    input_dir = Path(args.inputdir)
+    output_dir = Path(args.outputdir)
 
-    for filename in os.listdir(args.inputdir):
-        if fnmatch.fnmatch(filename, "*.src.png"):
-            basename = get_basename(filename)
+    if not input_dir.is_dir():
+        raise NotADirectoryError(f"{input_dir} is not a directory")
+
+    if not output_dir.is_dir():
+        from warnings import warn
+        warn(f"{output_dir} is not a directory, attempting to create it")
+        output_dir.mkdir(parents=True)
+
+    for file_path in input_dir.glob("*.*.png"):
+        suffix, _ = file_path.suffixes # Discard png suffix
+        if suffix == ".src":
+            basename = get_basename(file_path)
             maskname = f"{basename}.mask.png"
-            pngname = filename
 
-            pngRGB = Image.open(os.path.join(args.inputdir, pngname))
+            pngRGB = Image.open(file_path) # Also `input_dir / pngname`
 
-            maskRGB = Image.open(os.path.join(args.inputdir, maskname))
+            maskRGB = Image.open(file_path.with_name(maskname))
             maskMono = maskRGB.convert("1")
 
             newImage = Image.new("RGBA", pngRGB.size)
@@ -40,31 +60,38 @@ def main():
 
             composed = Image.alpha_composite(newImage, pngRGB)
 
-            composed.save(os.path.join(args.outputdir, f"{basename}.png"))
-        elif fnmatch.fnmatch(filename, "*.static.png"):
-            basename = get_basename(filename)
+            composed.save(output_dir / f"{basename}.png")
+        elif suffix == ".static":
+            basename = get_basename(file_path)
 
-            shutil.copyfile(os.path.join(args.inputdir, filename), os.path.join(args.outputdir, f"{basename}.png"))
+            shutil.copyfile(file_path, output_dir / f"{basename}.png")
 
-def get_basename(filename):
-    return filename[0:filename.find(".")]
 
-def parse_scheme(path):
-    scheme = dict()
+def get_basename(filename: Path) -> str:
+    """
+    Get the basename of a file without the extension.
+
+    Examples:
+        >>> get_basename(Path('example.tar.gz'))
+        'example'
+    """
+    return filename.stem.partition('.')[0]
+
+def parse_scheme(path: Path) -> dict:
+    """Parse a scheme file into a dictionary."""
+    scheme = {}
 
     with open(path) as schemefile:
         for line in schemefile:
             if line == "":
                 continue
 
-            equals_pos = line.find("=")
+            scheme_id, _, scheme_value = line.partition("=")
 
-            scheme_id = line[0:equals_pos]
-            scheme_value = line[equals_pos + 1:len(line)]
-
-            scheme[scheme_id] = scheme_value
+            scheme[scheme_id] = scheme_value.strip()
 
     return scheme
+
 
 if __name__ == "__main__":
     main()
