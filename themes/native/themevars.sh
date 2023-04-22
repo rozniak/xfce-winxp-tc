d3f7feda74a56d96f79ea10d3bcc57d2a60d3fba
Rory Fewell <roryf@oddmatics.uk>
2023-04-22 10:24:53 +0100

    Prelim: CMake build for native resources

--- themes/native/themevars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export THEME_BASENAME="Windows Classic style"
-export THEME_DESCRIPTION="Windows Classic visual style for XFCE."
-export THEME_MAINTAINER="Rory Fewell <roryf@oddmatics.uk>"
-export THEME_RAWNAME="native"
-export THEME_VARIANT="Default"
-
-export THEME_DISPLAYNAME="${THEME_BASENAME}"
