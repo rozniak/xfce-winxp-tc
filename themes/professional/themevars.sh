b8e97d68ce8549eb5e3d9ab5e2e79837464ce945
Rory Fewell <roryf@oddmatics.uk>
2023-04-22 23:35:29 +0100

    Prelim: CMake build for Professional theme

--- themes/professional/themevars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export THEME_BASENAME="Professional"
-export THEME_DESCRIPTION="Professional visual style for XFCE."
-export THEME_MAINTAINER="Rory Fewell <roryf@oddmatics.uk>"
-export THEME_RAWNAME="professional"
-export THEME_VARIANT="Default"
-
-export THEME_DISPLAYNAME="${THEME_BASENAME}"
