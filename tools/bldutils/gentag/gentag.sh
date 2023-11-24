a54b7c17739031eac40bd819026ab6ea27288d8b
Rory Fewell <roryf@oddmatics.uk>
2023-11-24 16:59:17 +0000

    Bugfix: Fixes #237, Build tag generation is scuffed when there's no git metadata

--- tools/bldutils/gentag/gentag.sh
+++ tools/bldutils/gentag/gentag.sh
@@ -1,9 +1,25 @@
 #/bin/bash
-cur_branch=`git branch --show-current | xargs echo -n`
+
+# Set up some defaults for the tag
+#
+cur_branch='unknown'
 cur_datestamp=`date +%y%m%d-%H%M | xargs echo -n`
-cur_hash=`git rev-parse --short HEAD | xargs echo -n`
+cur_hash='unknown'
 cur_user=`whoami | xargs echo -n`
 
+# Check we're in a git repo, if the user has downloaded as ZIP from jithub then
+# we won't have any metadata
+#
+git status >/dev/null 2>&1
+
+if [[ $? -eq 0 ]]
+then
+    cur_branch=`git branch --show-current | xargs echo -n`
+    cur_hash=`git rev-parse --short HEAD | xargs echo -n`
+fi
+
+# Construct and output the tag now
+#
 tag="${cur_hash}.${cur_branch}(${cur_user}).${cur_datestamp}"
 
 echo -n "${tag}"
