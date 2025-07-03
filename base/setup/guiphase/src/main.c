f895745ddbdcf7f3fdd20d838b1bea104f80d908
Rory Fewell <roryf@oddmatics.uk>
2025-07-04 00:20:36 +0100

    Prelim: Add wsetupx --arm, add to targets

--- base/setup/guiphase/src/main.c
+++ base/setup/guiphase/src/main.c
@@ -1,8 +1,10 @@
 #include <glib.h>
 #include <gtk/gtk.h>
+#include <stdlib.h>
 #include <wintc/comgtk.h>
 #include <wintc/shelldpa.h>
 
+#include "arm.h"
 #include "setupwnd.h"
 
 //
@@ -14,6 +16,24 @@ static void on_window_destroyed(
 );
 
 //
+// STATIC DATA
+//
+static gboolean S_OPTION_ARM = FALSE;
+
+static GOptionEntry S_ENTRIES[] = {
+    {
+        "arm",
+        'a',
+        0,
+        G_OPTION_ARG_NONE,
+        &S_OPTION_ARM,
+        "Arm the system to boot into graphical-mode setup.",
+        NULL
+    },
+    G_OPTION_ENTRY_NULL
+};
+
+//
 // ENTRY POINT
 //
 int main(
@@ -21,6 +41,51 @@ int main(
     char* argv[]
 )
 {
+    GError* error = NULL;
+
+    //
+    // Parse startup options
+    //
+    GOptionContext* ctx = g_option_context_new(NULL);
+
+    g_option_context_add_main_entries(
+        ctx,
+        S_ENTRIES,
+        NULL
+    );
+    g_option_context_add_group(
+        ctx,
+        gtk_get_option_group(TRUE)
+    );
+
+    if (!g_option_context_parse(ctx, &argc, &argv, &error))
+    {
+        wintc_log_error_and_clear(&error);
+        return EXIT_FAILURE;
+    }
+
+    //
+    // Handle options
+    //
+    if (S_OPTION_ARM)
+    {
+        gboolean res_arm = wintc_setup_arm_system();
+
+        if (res_arm)
+        {
+            g_message("%s", "Setup prepared, reboot to begin setup.");
+        }
+        else
+        {
+            g_critical("%s", "Setup preparation was unsuccessful.");
+        }
+
+        return res_arm ? EXIT_SUCCESS : EXIT_FAILURE;
+    }
+
+    //
+    // Continue to normal graphical mode setup
+    //
     gtk_init(&argc, &argv);
 
     if (!wintc_init_display_protocol_apis())
@@ -69,7 +134,7 @@ int main(
     gtk_widget_show_all(window);
     gtk_main();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 //
