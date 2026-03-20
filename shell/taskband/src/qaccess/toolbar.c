3374f7b6acfd56dec7c6a7c9389649803974698c
Rory Fewell <roryf@oddmatics.uk>
2026-03-20 19:35:20 +0000

    [#441] Scaffold file monitor for quick-launch profile dir

--- shell/taskband/src/qaccess/toolbar.c
+++ shell/taskband/src/qaccess/toolbar.c
@@ -6,12 +6,27 @@
 #include "../intapi.h"
 #include "toolbar.h"
 
+#define WINTC_COMPONENT_QUICK_ACCESS "quick-launch"
+
 //
 // FORWARD DECLARATIONS
 //
 static void wintc_toolbar_quick_access_constructed(
     GObject* object
 );
+
+static void on_monitor_dir_changed(
+    GFileMonitor*     self,
+    GFile*            file,
+    GFile*            other_file,
+    GFileMonitorEvent event_type,
+    gpointer          user_data
+);
+
+//
+// STATIC DATA
+//
+static gchar* S_DIR_QUICK_ACCESS = NULL;
 
 //
 // GTK OOP CLASS/INSTANCE DEFINITIONS
@@ -23,6 +38,10 @@ struct _WinTCToolbarQuickAccess
     // UI stuff
     //
     GtkWidget* box_programs;
+
+    // Dir monitor stuff
+    //
+    GFileMonitor* monitor_dir;
 };
 
 //
@@ -41,11 +60,51 @@ static void wintc_toolbar_quick_access_class_init(
     GObjectClass* object_class = G_OBJECT_CLASS(klass);
 
     object_class->constructed = wintc_toolbar_quick_access_constructed;
+
+    // Sort out profile
+    //
+    S_DIR_QUICK_ACCESS =
+        wintc_profile_get_path(WINTC_COMPONENT_QUICK_ACCESS, "");
+
+    if (!wintc_profile_ensure_exists(WINTC_COMPONENT_QUICK_ACCESS, NULL))
+    {
+        g_warning(
+            "taskband: qaccess: failed to create dir: %s",
+            S_DIR_QUICK_ACCESS
+        );
+    }
 }
 
 static void wintc_toolbar_quick_access_init(
-    WINTC_UNUSED(WinTCToolbarQuickAccess* self)
-) {}
+    WinTCToolbarQuickAccess* self
+)
+{
+    GError* error        = NULL;
+    GFile*  file_qaccess = g_file_new_for_path(S_DIR_QUICK_ACCESS);
+
+    self->monitor_dir =
+        g_file_monitor_directory(
+            file_qaccess,
+            G_FILE_MONITOR_NONE,
+            NULL,
+            &error
+        );
+
+    g_object_unref(file_qaccess);
+
+    if (!(self->monitor_dir))
+    {
+        wintc_log_error_and_clear(&error);
+        return;
+    }
+
+    g_signal_connect(
+        self->monitor_dir,
+        "changed",
+        G_CALLBACK(on_monitor_dir_changed),
+        self
+    );
+}
 
 //
 // CLASS VIRTUAL METHODS
@@ -83,4 +142,22 @@ static void wintc_toolbar_quick_access_constructed(
     );
 
     gtk_widget_show(button);
+}
+
+//
+// CALLBACKS
+//
+static void on_monitor_dir_changed(
+    WINTC_UNUSED(GFileMonitor* self),
+    GFile*            file,
+    WINTC_UNUSED(GFile* other_file),
+    GFileMonitorEvent event_type,
+    WINTC_UNUSED(gpointer user_data)
+)
+{
+    g_message(
+        "toolbar: qaccess: dir monitor update (%d): %s",
+        event_type,
+        g_file_peek_path(file)
+    );
 }
