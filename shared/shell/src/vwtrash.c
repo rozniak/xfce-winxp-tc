3cf50ef786b63eb4f968cda677e4ed5108a57a60
Rory Fewell <roryf@oddmatics.uk>
2026-05-28 22:46:52 +0100

    [#589] Define Recycle Bin suggested actions

--- shared/shell/src/vwtrash.c
+++ shared/shell/src/vwtrash.c
@@ -78,6 +78,10 @@ static void wintc_sh_view_trash_get_path(
     WinTCIShextView*    view,
     WinTCShextPathInfo* path_info
 );
+static GMenuModel* wintc_sh_view_trash_get_suggested_actions(
+    WinTCIShextView* view,
+    guint            item_hash
+);
 static guint wintc_sh_view_trash_get_unique_hash(
     WinTCIShextView* view
 );
@@ -251,6 +255,7 @@ static void wintc_sh_view_trash_ishext_view_interface_init(
         wintc_sh_view_trash_get_operations_for_view;
     iface->get_parent_path         = wintc_sh_view_trash_get_parent_path;
     iface->get_path                = wintc_sh_view_trash_get_path;
+    iface->get_suggested_actions   = wintc_sh_view_trash_get_suggested_actions;
     iface->get_unique_hash         = wintc_sh_view_trash_get_unique_hash;
     iface->has_parent              = wintc_sh_view_trash_has_parent;
     iface->refresh_items           = wintc_sh_view_trash_refresh_items;
@@ -450,6 +455,43 @@ static void wintc_sh_view_trash_get_path(
         g_strdup(
             wintc_sh_get_place_path(WINTC_SH_PLACE_RECYCLEBIN)
         );
+}
+
+static GMenuModel* wintc_sh_view_trash_get_suggested_actions(
+    WINTC_UNUSED(WinTCIShextView* view),
+    guint item_hash
+)
+{
+    if (item_hash)
+    {
+        g_critical(
+            "%s",
+            "shell: vwtrash suggested actions for item not implemented"
+        );
+
+        return NULL;
+    }
+
+    // Construct the suggested actions menu UI
+    //
+    GtkBuilder* builder =
+        gtk_builder_new_from_resource("/uk/oddmatics/wintc/shell/amtrshvw.ui");
+
+    GMenuModel* menu = NULL;
+
+    wintc_lc_builder_preprocess_widget_text(builder);
+
+    wintc_builder_get_objects(
+        builder,
+        "menu", &menu,
+        NULL
+    );
+
+    g_object_ref(menu);
+
+    g_object_unref(builder);
+
+    return menu;
 }
 
 static guint wintc_sh_view_trash_get_unique_hash(
