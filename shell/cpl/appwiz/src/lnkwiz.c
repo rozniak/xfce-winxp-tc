bd448ce1dda13cb712a88cb0eaeb065140d9d7a7
Rory Fewell <roryf@oddmatics.uk>
2026-06-06 17:33:12 +0100

    [#601] Implement first page logic of New Link Here wizard

--- shell/cpl/appwiz/src/lnkwiz.c
+++ shell/cpl/appwiz/src/lnkwiz.c
@@ -1,6 +1,7 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <wintc/comgtk.h>
+#include <wintc/shlang.h>
 #include <wintc/wizard97.h>
 
 #include "lnkwiz.h"
@@ -12,12 +13,15 @@ enum
 {
     PROP_NULL,
     PROP_PATH,
-    N_PROPERTIES
+    N_PROPERTIES,
+
+    OVERRIDE_PROP_CAN_NEXT,
 };
 
 enum
 {
     WIZPAGE_INTRO,
+    WIZPAGE_FINISH
 };
 
 //
@@ -32,6 +36,12 @@ static void wintc_cpl_appwiz_new_link_wizard_dispose(
 static void wintc_cpl_appwiz_new_link_wizard_finalize(
     GObject* object
 );
+static void wintc_cpl_appwiz_new_link_wizard_get_property(
+    GObject*    object,
+    guint       prop_id,
+    GValue*     value,
+    GParamSpec* pspec
+);
 static void wintc_cpl_appwiz_new_link_wizard_set_property(
     GObject*      object,
     guint         prop_id,
@@ -43,6 +53,32 @@ static void wintc_cpl_appwiz_new_link_wizard_constructing_page(
     WinTCWizard97Window* wiz_wnd,
     guint                page_num,
     GtkBuilder*          builder
+);
+static void wintc_cpl_appwiz_new_link_wizard_presenting_page(
+    WinTCWizard97Window* wiz_wnd,
+    guint                page_num
+);
+static gboolean wintc_cpl_appwiz_new_link_wizard_validate(
+    WinTCWizard97Window* wiz_wnd,
+    guint                current_page
+);
+
+static void wintc_cpl_appwiz_new_link_wizard_update_state(
+    WinTCCplAppwizNewLinkWizard* lnkwiz,
+    guint                        page_num
+);
+
+static void on_button_browse_clicked(
+    GtkWidget* button,
+    gpointer   user_data
+);
+static void on_entry_name_changed(
+    GtkEditable* editable,
+    gpointer     user_data
+);
+static void on_entry_target_changed(
+    GtkEditable* editable,
+    gpointer     user_data
 );
 
 //
@@ -65,6 +101,8 @@ struct _WinTCCplAppwizNewLinkWizard
 
     // State
     //
+    gboolean can_next;
+
     GFile* file;
     gchar* path;
 
@@ -72,6 +110,8 @@ struct _WinTCCplAppwizNewLinkWizard
     //
     GtkWidget* button_browse;
     GtkWidget* entry_target;
+
+    GtkWidget* entry_name;
 };
 
 //
@@ -94,10 +134,18 @@ static void wintc_cpl_appwiz_new_link_wizard_class_init(
     object_class->constructed  = wintc_cpl_appwiz_new_link_wizard_constructed;
     object_class->dispose      = wintc_cpl_appwiz_new_link_wizard_dispose;
     object_class->finalize     = wintc_cpl_appwiz_new_link_wizard_finalize;
+    object_class->get_property = wintc_cpl_appwiz_new_link_wizard_get_property;
     object_class->set_property = wintc_cpl_appwiz_new_link_wizard_set_property;
     wizard_class->constructing_page =
         wintc_cpl_appwiz_new_link_wizard_constructing_page;
+    wizard_class->presenting_page   =
+        wintc_cpl_appwiz_new_link_wizard_presenting_page;
+    wizard_class->validate          =
+        wintc_cpl_appwiz_new_link_wizard_validate;
 
+
+    // Set up properties
+    //
     wintc_cpl_appwiz_new_link_wizard_properties[PROP_PATH] =
         g_param_spec_string(
             "path",
@@ -113,6 +161,14 @@ static void wintc_cpl_appwiz_new_link_wizard_class_init(
         wintc_cpl_appwiz_new_link_wizard_properties
     );
 
+    g_object_class_override_property(
+        object_class,
+        OVERRIDE_PROP_CAN_NEXT,
+        "can-next"
+    );
+
+    // Configure wizard
+    //
     wintc_wizard97_window_class_setup_from_resources(
         wizard_class,
         "/uk/oddmatics/wintc/appwiz/watermk.png",
@@ -127,6 +183,8 @@ static void wintc_cpl_appwiz_new_link_wizard_init(
     WinTCCplAppwizNewLinkWizard* self
 )
 {
+    self->can_next = FALSE;
+
     wintc_wizard97_window_init_wizard(
         WINTC_WIZARD97_WINDOW(self)
     );
@@ -176,6 +234,28 @@ static void wintc_cpl_appwiz_new_link_wizard_finalize(
         ->finalize(object);
 }
 
+static void wintc_cpl_appwiz_new_link_wizard_get_property(
+    GObject*    object,
+    guint       prop_id,
+    GValue*     value,
+    GParamSpec* pspec
+)
+{
+    WinTCCplAppwizNewLinkWizard* lnkwiz =
+        WINTC_CPL_APPWIZ_NEW_LINK_WIZARD(object);
+
+    switch (prop_id)
+    {
+        case OVERRIDE_PROP_CAN_NEXT:
+            g_value_set_boolean(value, lnkwiz->can_next);
+            break;
+
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+            break;
+    }
+}
+
 static void wintc_cpl_appwiz_new_link_wizard_set_property(
     GObject*      object,
     guint         prop_id,
@@ -217,9 +297,90 @@ static void wintc_cpl_appwiz_new_link_wizard_constructing_page(
                 NULL
             );
 
+            g_signal_connect(
+                lnkwiz->button_browse,
+                "clicked",
+                G_CALLBACK(on_button_browse_clicked),
+                lnkwiz
+            );
+            g_signal_connect(
+                lnkwiz->entry_target,
+                "changed",
+                G_CALLBACK(on_entry_target_changed),
+                lnkwiz
+            );
+
+            break;
+
+        case WIZPAGE_FINISH:
+            wintc_builder_get_objects(
+                builder,
+                "entry-name", &(lnkwiz->entry_name),
+                NULL
+            );
+
+            g_signal_connect(
+                lnkwiz->entry_name,
+                "changed",
+                G_CALLBACK(on_entry_name_changed),
+                lnkwiz
+            );
+
             break;
 
         default: break;
+    }
+}
+
+static void wintc_cpl_appwiz_new_link_wizard_presenting_page(
+    WinTCWizard97Window* wiz_wnd,
+    guint                page_num
+)
+{
+    wintc_cpl_appwiz_new_link_wizard_update_state(
+        WINTC_CPL_APPWIZ_NEW_LINK_WIZARD(wiz_wnd),
+        page_num
+    );
+}
+
+static gboolean wintc_cpl_appwiz_new_link_wizard_validate(
+    WinTCWizard97Window* wiz_wnd,
+    guint                current_page
+)
+{
+    WinTCCplAppwizNewLinkWizard* lnkwiz =
+        WINTC_CPL_APPWIZ_NEW_LINK_WIZARD(wiz_wnd);
+
+    switch (current_page)
+    {
+        case WIZPAGE_INTRO:
+        {
+            const gchar* target =
+                gtk_entry_get_text(GTK_ENTRY(lnkwiz->entry_target));
+
+            if (!g_file_test(target, G_FILE_TEST_EXISTS))
+            {
+                gchar* msg =
+                    g_strdup_printf("The file %s cannot be found.", target);
+
+                wintc_messagebox_show(
+                    GTK_WINDOW(lnkwiz),
+                    msg,
+                    "Create Shortcut",
+                    GTK_BUTTONS_OK,
+                    GTK_MESSAGE_WARNING
+                );
+
+                g_free(msg);
+
+                return FALSE;
+            }
+
+            return TRUE;
+        }
+
+        default:
+            return TRUE;
     }
 }
 
@@ -243,3 +404,137 @@ GtkWidget* wintc_cpl_appwiz_new_link_wizard_new(
 //
 // PRIVATE FUNCTIONS
 //
+static void wintc_cpl_appwiz_new_link_wizard_update_state(
+    WinTCCplAppwizNewLinkWizard* lnkwiz,
+    guint                        page_num
+)
+{
+    gboolean can_next = TRUE;
+
+    switch (page_num)
+    {
+        case WIZPAGE_INTRO:
+            can_next =
+                strlen(
+                    gtk_entry_get_text(GTK_ENTRY(lnkwiz->entry_target))
+                ) > 0;
+
+            break;
+
+        case WIZPAGE_FINISH:
+            can_next =
+                strlen(
+                    gtk_entry_get_text(GTK_ENTRY(lnkwiz->entry_name))
+                ) > 0;
+
+            break;
+
+        default: break;
+    }
+
+    lnkwiz->can_next = can_next;
+
+    g_object_notify(
+        G_OBJECT(lnkwiz),
+        "can-next"
+    );
+}
+
+//
+// CALLBACKS
+//
+static void on_button_browse_clicked(
+    GtkWidget* button,
+    gpointer   user_data
+)
+{
+    WinTCCplAppwizNewLinkWizard* lnkwiz =
+        WINTC_CPL_APPWIZ_NEW_LINK_WIZARD(user_data);
+
+    // Try to figure out where we should open
+    //
+    const char* target   = gtk_entry_get_text(GTK_ENTRY(lnkwiz->entry_target));
+    gchar*      open_dir = NULL;
+
+    if (strlen(target) > 0 && *target == '/')
+    {
+        open_dir = g_path_get_dirname(target);
+    }
+    else
+    {
+        open_dir =
+            g_strdup(g_get_user_special_dir(G_USER_DIRECTORY_DOCUMENTS));
+    }
+
+    // Set up All Files filter
+    //
+    GtkFileFilter* filter_all_files = gtk_file_filter_new();
+
+    gtk_file_filter_set_name(filter_all_files, "All Files");
+    gtk_file_filter_add_pattern(filter_all_files, "*");
+
+    // Set up the file dialog
+    //
+    GtkWidget* dlg =
+        gtk_file_chooser_dialog_new(
+            wintc_lc_get_control_text(WINTC_CTLTXT_BROWSE, WINTC_PUNC_NONE),
+            wintc_widget_get_toplevel_window(button),
+            GTK_FILE_CHOOSER_ACTION_OPEN,
+            wintc_lc_get_control_text(WINTC_CTLTXT_CANCEL, WINTC_PUNC_NONE),
+            GTK_RESPONSE_CANCEL,
+            wintc_lc_get_control_text(WINTC_CTLTXT_OPEN, WINTC_PUNC_NONE),
+            GTK_RESPONSE_ACCEPT,
+            NULL
+        );
+
+    gtk_file_chooser_add_filter(
+        GTK_FILE_CHOOSER(dlg),
+        filter_all_files
+    );
+    gtk_file_chooser_set_current_folder(
+        GTK_FILE_CHOOSER(dlg),
+        open_dir
+    );
+
+    // Launch dialog now
+    //
+    if (gtk_dialog_run(GTK_DIALOG(dlg)) == GTK_RESPONSE_ACCEPT)
+    {
+        gtk_entry_set_text(
+            GTK_ENTRY(lnkwiz->entry_target),
+            gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dlg))
+        );
+    }
+
+    gtk_widget_destroy(dlg);
+
+    g_free(open_dir);
+}
+
+static void on_entry_name_changed(
+    WINTC_UNUSED(GtkEditable* editable),
+    gpointer user_data
+)
+{
+    WinTCCplAppwizNewLinkWizard* lnkwiz =
+        WINTC_CPL_APPWIZ_NEW_LINK_WIZARD(user_data);
+
+    wintc_cpl_appwiz_new_link_wizard_update_state(
+        lnkwiz,
+        WIZPAGE_FINISH
+    );
+}
+
+static void on_entry_target_changed(
+    WINTC_UNUSED(GtkEditable* editable),
+    gpointer user_data
+)
+{
+    WinTCCplAppwizNewLinkWizard* lnkwiz =
+        WINTC_CPL_APPWIZ_NEW_LINK_WIZARD(user_data);
+
+    wintc_cpl_appwiz_new_link_wizard_update_state(
+        lnkwiz,
+        WIZPAGE_INTRO
+    );
+}
