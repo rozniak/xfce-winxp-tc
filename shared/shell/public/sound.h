cf9fb6994ecffce5e0e6387f71cbb2aa49dd71eb
Rory Fewell <roryf@oddmatics.uk>
2026-06-08 21:53:13 +0100

    [#606] Use comgtk sound playback in shell lib

--- shared/shell/public/sound.h
+++ shared/shell/public/sound.h
@@ -4,49 +4,52 @@
 #include <glib.h>
 
 //
-// PUBLIC DEFINES
+// PUBLIC ENUMS
 //
-#define WINTC_SHELL_SND_ASTERISK "dialog-warning"
-#define WINTC_SHELL_SND_BATTCRIT "battery-low"
-#define WINTC_SHELL_SND_CRITSTOP "dialog-error"
-#define WINTC_SHELL_SND_CLOSEPRG "windows-close-program"
-#define WINTC_SHELL_SND_DEFAULT  "bell-window-system"
-#define WINTC_SHELL_SND_DEVADD   "device-added-media"
-#define WINTC_SHELL_SND_DEVREM   "device-removed-media"
-#define WINTC_SHELL_SND_DEVFAIL  "windows-hardfail"
-#define WINTC_SHELL_SND_EXCLAM   "dialog-question"
-#define WINTC_SHELL_SND_EXITWIN  "desktop-logout"
-#define WINTC_SHELL_SND_BATTLOW  "battery-caution"
-#define WINTC_SHELL_SND_MAXIMIZE "windows-maximize"
-#define WINTC_SHELL_SND_MENUCMD  "windows-menu-command"
-#define WINTC_SHELL_SND_MENUPOP  "windows-menu-popup"
-#define WINTC_SHELL_SND_MINIMIZE "windows-minimize"
-#define WINTC_SHELL_SND_NEWMAIL  "message-new-email"
-#define WINTC_SHELL_SND_OPENPRG  "windows-open-program"
-#define WINTC_SHELL_SND_PRINTCMP "windows-print-complete"
-#define WINTC_SHELL_SND_PROGERR  "windows-program-error"
-#define WINTC_SHELL_SND_QUESTION "windows-question"
-#define WINTC_SHELL_SND_RESTDOWN "windows-restore-down"
-#define WINTC_SHELL_SND_RESTUP   "windows-restore-up"
-#define WINTC_SHELL_SND_SELECT   "windows-select"
-#define WINTC_SHELL_SND_SHOWTOOL "windows-show-toolbar-band"
-#define WINTC_SHELL_SND_STARTWIN "desktop-login"
-#define WINTC_SHELL_SND_SYSNOTIF "windows-system-notification"
-#define WINTC_SHELL_SND_LOGOFF   "windows-logoff"
-#define WINTC_SHELL_SND_LOGON    "windows-logon"
+typedef enum _WinTCShSound
+{
+    WINTC_SHELL_SND_ASTERISK,
+    WINTC_SHELL_SND_BATTCRIT,
+    WINTC_SHELL_SND_CRITSTOP,
+    WINTC_SHELL_SND_CLOSEPRG,
+    WINTC_SHELL_SND_DEFAULT,
+    WINTC_SHELL_SND_DEVADD,
+    WINTC_SHELL_SND_DEVREM,
+    WINTC_SHELL_SND_DEVFAIL,
+    WINTC_SHELL_SND_EXCLAM,
+    WINTC_SHELL_SND_EXITWIN,
+    WINTC_SHELL_SND_BATTLOW,
+    WINTC_SHELL_SND_MAXIMIZE,
+    WINTC_SHELL_SND_MENUCMD,
+    WINTC_SHELL_SND_MENUPOP,
+    WINTC_SHELL_SND_MINIMIZE,
+    WINTC_SHELL_SND_NEWMAIL,
+    WINTC_SHELL_SND_OPENPRG,
+    WINTC_SHELL_SND_PRINTCMP,
+    WINTC_SHELL_SND_PROGERR,
+    WINTC_SHELL_SND_QUESTION,
+    WINTC_SHELL_SND_RESTDOWN,
+    WINTC_SHELL_SND_RESTUP,
+    WINTC_SHELL_SND_SELECT,
+    WINTC_SHELL_SND_SHOWTOOL,
+    WINTC_SHELL_SND_STARTWIN,
+    WINTC_SHELL_SND_SYSNOTIF,
+    WINTC_SHELL_SND_LOGOFF,
+    WINTC_SHELL_SND_LOGON,
 
-#define WINTC_SHELL_SND_BLKPOPUP "windows-blocked-popup"
-#define WINTC_SHELL_SND_CMPNAV   "windows-complete-navigation"
-#define WINTC_SHELL_SND_EMPTYBIN "trash-empty"
-#define WINTC_SHELL_SND_INFOBAR  "windows-information-bar"
-#define WINTC_SHELL_SND_MOVEITEM "windows-move-menu-item"
-#define WINTC_SHELL_SND_STARTNAV "windows-start-navigation"
+    WINTC_SHELL_SND_BLKPOPUP,
+    WINTC_SHELL_SND_CMPNAV,
+    WINTC_SHELL_SND_EMPTYBIN,
+    WINTC_SHELL_SND_INFOBAR,
+    WINTC_SHELL_SND_MOVEITEM,
+    WINTC_SHELL_SND_STARTNAV
+} WinTCShSound;
 
 //
 // PUBLIC FUNCTIONS
 //
 void wintc_sh_play_sound(
-    const gchar* name
+    WinTCShSound sound
 );
 
 #endif
--- shared/shell/src/sound.c
+++ shared/shell/src/sound.c
@@ -1,22 +1,55 @@
-#include <canberra.h>
-#include <canberra-gtk.h>
 #include <glib.h>
+#include <wintc/comgtk.h>
 
 #include "../public/sound.h"
+
+//
+// STATIC DATA
+//
+static const gchar* S_XDG_SOUNDS[] = {
+    "dialog-warning",
+    "battery-low",
+    "dialog-error",
+    "windows-close-program",
+    "bell-window-system",
+    "device-added-media",
+    "device-removed-media",
+    "windows-hardfail",
+    "dialog-question",
+    "desktop-logout",
+    "battery-caution",
+    "windows-maximize",
+    "windows-menu-command",
+    "windows-menu-popup",
+    "windows-minimize",
+    "message-new-email",
+    "windows-open-program",
+    "windows-print-complete",
+    "windows-program-error",
+    "windows-question",
+    "windows-restore-down",
+    "windows-restore-up",
+    "windows-select",
+    "windows-show-toolbar-band",
+    "desktop-login",
+    "windows-system-notification",
+    "windows-logoff",
+    "windows-logon",
+
+    "windows-blocked-popup",
+    "windows-complete-navigation",
+    "trash-empty",
+    "windows-information-bar",
+    "windows-move-menu-item",
+    "windows-start-navigation"
+};
 
 //
 // PUBLIC FUNCTIONS
 //
 void wintc_sh_play_sound(
-    const gchar* name
+    WinTCShSound sound
 )
 {
-    ca_context* ctx = ca_gtk_context_get();
-
-    ca_context_play(
-        ctx,
-        0,
-        CA_PROP_EVENT_ID, name,
-        NULL
-    );
+    wintc_xdg_play_sound(S_XDG_SOUNDS[sound]);
 }
